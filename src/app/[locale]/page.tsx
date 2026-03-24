bfb8b022122260810b61b96c489bd277de99bc44
Curtis <ildoonet@gmail.com>
2026-03-25 06:52:55 +0900

    revert: 홈페이지 원래대로 복구

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,13 @@
+import Header from "@/components/layout/Header";
+import HomeShell from "@/components/home/HomeShell";
+
 export default function HomePage() {
-  return <div className="min-h-screen bg-white" />;
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <main className="max-w-6xl mx-auto px-4 pb-40 page-top">
+        <HomeShell />
+      </main>
+    </div>
+  );
 }
